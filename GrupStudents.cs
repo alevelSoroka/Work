46ce90951a7927b16fb1323043c2ca96131aa17b
alevelSoroka <41588991+alevelSoroka@users.noreply.github.com>
2018-08-02 10:32:18 +0300

    Add files via upload

---
+++ GrupStudents.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class GrupStudents
+    {
+        List<Student> _students = new List<Student>();
+
+        public bool AddStudent(Student s)
+        {
+            if (_students.Count >= MaxCount)
+                return false;
+            _students.Add(s);
+            return true;
+        }
+
+        public List<Student> GetStudent()
+        {
+            return _students;
+        }
+
+        public int GetStudentCount()
+        {
+
+            return _students.Count;
+        }
+
+        int MaxCount = 2;
+    }
+}
