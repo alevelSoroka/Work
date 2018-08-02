b1a04d92424d48427e9050d59791ecb3f1323d7b
alevelSoroka <41588991+alevelSoroka@users.noreply.github.com>
2018-08-02 10:29:19 +0300

    Add files via upload

---
+++ Repository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Repository
+    {
+        List<GrupStudents> _grups = new List<GrupStudents>();
+        List<Prepod> _prepods = new List<Prepod>();
+
+        public bool AddStudent(Student s)
+        {
+            foreach (var g in _grups)
+            {
+                if (g.AddStudent(s))
+                    return true;
+            }
+            var grup = new GrupStudents();
+            grup.AddStudent(s);
+            _grups.Add(grup);
+            return false;
+        }
+
+        public void AddPrepod(Prepod p)
+        {
+            _prepods.Add(p);
+        }
+
+        public List<Student> GetStudent()
+        {
+            var students = new List<Student>();
+            foreach (var g in _grups)
+            {
+                students.AddRange(g.GetStudent());
+
+            }
+            return students;
+        }
+
+        public List<Prepod> GetPrepod()
+        {
+            return _prepods;
+        }
+
+        public List<GrupStudents> GetGrups()
+        {
+            return _grups;
+        }
+
+
+        
+    }
+}
